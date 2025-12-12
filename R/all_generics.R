e22f5012ece7f5d22abaddcbed1d5e2a99b9f0d0
thomaz <thomazbastiaanssen@gmail.com>
2025-12-12 16:55:20 +0100

    up - moving towards tidy style function names

---
+++ R/all_generics.R
@@ -0,0 +1,6 @@
+#' #' @title Create a dataset
+#' #' @export
+#' #' @param x an R object.
+#' #' @examples
+#' #' create_dataset
+#' create_dataset <- S7::new_generic("create_dataset", "x")
--- R/create_dataset.R
+++ R/create_dataset.R
@@ -10,43 +10,68 @@ meta_add <- function(x, type) {
 #'     should be created.
 #' @param engine `function` The function that will write out the content of `x`.
 #'     (Default: `arrow::write_parquet`)
-#' @param ... Additional arguments to function specified with 'engine' argument.
+#' @param engine_args `list` of other arguments to `engine` function argument.
+#'     (Default: NULL)
 #' @param .file_suffix,.file_ext `Character scalar`. Specify file suffix and
 #'     file extension, respectively. (Default: `NULL`)
 #' @export
 #' @importFrom arrow write_parquet
 #'
 create_dataset <- function(
-        x, dataset_name, engine = arrow::write_parquet, ...,
+        x, dataset_name, engine = arrow::write_parquet, engine_args = NULL,
         .file_suffix = NULL, .file_ext = NULL
         ) {
     # check input
-    stopifnot("'x' must be a named list. " = is.list(x) && length(names) > 0L)
+    stopifnot(
+        "'x' must be a character vector or a named list." =
+            inherits(x, c("list", "character"))
+        )
+    # Simple case first
+    if( is.character(x) ) {
+        .build_dataset_folders(x, dataset_name)
+        return(invisible())
+        }
+    if( is.null(x_names <- names(x)) ) {
+        stop("'x' must be a character vector or a named list.")
+    }
+    .build_dataset_folders(x_names, dataset_name)
 
     # Guess file extension from writing engine
     if(is.null(.file_ext)) {
         .file_ext <- .engine2extension(substitute(engine))
     }
 
-    dir.create(dataset_name)
-
-    for( n in names(x) ) {
-       dir.create(file.path(dataset_name, n))
-    }
-
     out_paths <- paste0(
-        file.path(dataset_name, names(x), paste0(names(x), .file_suffix)),
+        file.path(dataset_name, x_names, paste0(x_names, .file_suffix)),
         .file_ext
         )
 
-    if( length(dargs <- list(...)) < 1L ) {
-        mapply( FUN = engine, x, out_paths, SIMPLIFY = FALSE )
-    } else {
-        mapply( FUN = engine, x, out_paths, MoreArgs = dargs, SIMPLIFY = FALSE )
-    }
+    .add_dataset_files(x, out_paths, engine, engine_args)
 
     invisible(NULL)
 }
+
+#' @param x a character `vector names(x)``
+#' @param dataset_name from `create_dataset()` call
+#' @noRd
+#'
+.build_dataset_folders <- function(x, dataset_name) {
+    dir.create(dataset_name)
+
+    for( n in x ) {dir.create( file.path(dataset_name, n) )}
+
+    invisible(NULL)
+}
+
+#' @param x a list of files to write
+#' @param out_paths character vector of out paths
+#' @param engine which function to use for this.
+#' @noRd
+#'
+.add_dataset_files <- function(x, out_paths, engine, engine_args) .mapply(
+    engine, dots = list(x, out_paths), MoreArgs = engine_args
+    )
+
 
 .engine2extension <- function(x) {
     # Compare engine to string of extensions
